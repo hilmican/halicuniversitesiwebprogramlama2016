d8ba4f8a33a1ae0478719be256c7b53462afd11e
hbaycan <hilmi.baycan@mediatriple.com>
2016-11-03 15:46:27 +0300

    girisler guncellenmis

--- HILMI/OgrenciSistemi/giris.php
+++ HILMI/OgrenciSistemi/giris.php
@@ -5,9 +5,11 @@
   </head>
   <body>
   	<form action="http://halicders.mediatriple.net/HILMI/OgrenciSistemi/giris_islem.php" method="POST">
-  		<font color="blue">İsim:</font><input type="text" name="isim">
+  		<font color="blue">İsim:</font>
+      <input type="text" name="isim">
   		<br>
-  		Şifre:<input type="text" name="sifre">
+  		Şifre:
+      <input type="text" name="sifre">
   		<br>
   		<input type="submit" value="Gönder">
   	</form>
